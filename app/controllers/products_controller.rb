2aa467ff14a9a451c77500b814dc136751c65c00
Rosa Santiago <rosa.d.santiago85@gmail.com>
2017-07-09 11:50:44 -0500

    adding id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+def index
+  @products = Product.all
+  render "product.html.erb"
+end
+
+def index
+  @products = Product.all 
+  render "all_products.html.erb"
+end
+
+def show
+  products_id = params[:id]
+  @products = params.find_by(id: recipe_id)
+  render "show.html.erb"
+end
+
 end
